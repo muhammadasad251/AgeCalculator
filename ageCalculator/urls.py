14fe40e018905d734753ce61421399ce65c9da70
Muhammad Asad <92978676+muhammadasad251@users.noreply.github.com>
2022-03-18 02:53:24 +0000

    All changes has been done and all basic functionalities are being performed here.
    All changes has been done and all basic functionalities are being performed here.

--- ageCalculator/urls.py
+++ ageCalculator/urls.py
@@ -3,4 +3,7 @@ from ageCalculator.views import *
 
 urlpatterns =[
     path('', index, name='index'),
+    path('index', index, name='index'),
+    path('rangeCalculator',rangeCalculator, name='rangeCalculator'),
+    path('weekDays',weekDays, name='weekDays')
 ]
--- ageCalculator/views.py
+++ ageCalculator/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from datetime import date, datetime
+import numpy as np
 
 # Create your views here.
 def index(request):
@@ -32,3 +33,58 @@ def index(request):
             "age":age
         }
     return render(request, 'index.html',context)
+
+
+
+def rangeCalculator(request):
+    context={}
+    if request.method =="POST":
+        date_of_birth = request.POST['sdate']
+        edate = request.POST['edate']
+        def Calc(date_of_birth,edate):
+            born_date = date_of_birth
+            born = datetime.strptime(born_date, '%Y-%m-%d')
+            today = edate
+            today=datetime.strptime(today, '%Y-%m-%d')
+            curr_month = today.month
+            born_month = born.month
+            curr_day   = today.day
+            born_day   = born.day
+            if born_month > curr_month:
+                month_diff = born_month - curr_month
+            else :
+                month_diff = curr_month - born_month
+            if born_day > curr_day:
+                day_diff = born_day - curr_day
+            else :
+                day_diff = curr_day - born_day 
+            age = today.year - born.year
+            print(str(age))
+            print(month_diff)
+            print(str(day_diff))
+            return " Your Age  : " + str(age) +"  Years   " + str(month_diff) +  "  Months  and " + str(day_diff) + "  Days"
+        age=(Calc(date_of_birth,edate))
+        context = {
+            "age":age
+        }
+        working_days=np.busday_count('2022-03-01', '2022-03-18')
+        print(working_days)
+    return render(request, 'rangeCalculator.html',context)
+
+def weekDays(request):
+    context={}
+    if request.method =="POST":
+        date_of_birth = request.POST['sdate']
+        edate = request.POST['edate']
+        def Calc(date_of_birth,edate):
+            born_date = date_of_birth
+            born = datetime.strptime(born_date, '%Y-%m-%d')
+            today = edate
+            today=datetime.strptime(today, '%Y-%m-%d')
+            days=working_days=np.busday_count(date_of_birth, edate)
+            return " Your Working days are  : " +str(days) + "  Days"
+        age=(Calc(date_of_birth,edate))
+        context = {
+            "age":age
+        }
+    return render(request, 'weekDays.html',context)
